41b5e3740bee739469516830211d111ada6b9274
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2017-08-03 16:11:49 +0530

    created the MainActivity and activity_main. There are some issues with the latest api

--- app/src/main/java/com/example/jaspreetsingh/piechart/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/piechart/MainActivity.java
@@ -1,13 +1,95 @@
 package com.example.jaspreetsingh.piechart;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements OnChartValueSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PieChart pieChart = (PieChart) findViewById(R.id.piechart);
+        pieChart.setUsePercentValues(true);
+
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        ArrayList<Entry> yvalues = new ArrayList<Entry>();
+        yvalues.add(new Entry(1f, 0));
+        yvalues.add(new Entry(1f, 1));
+        yvalues.add(new Entry(27f, 2));
+        yvalues.add(new Entry(38f, 3));
+        yvalues.add(new Entry(33f, 4));
+
+        PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        xVals.add("January");
+        xVals.add("February");
+        xVals.add("March");
+        xVals.add("April");
+        xVals.add("May");
+
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        pieChart.setData(data);
+        pieChart.setDescription("This is Pie Chart");
+
+        // Disable the rotation of the pie chart
+        pieChart.setRotationEnabled(false);
+
+        // Disable the hole in the pieChart
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.setTransparentCircleRadius(25f);
+        pieChart.setHoleRadius(25f);
+
+        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        data.setValueTextSize(13f);
+        data.setValueTextColor(Color.DKGRAY);
+        pieChart.setOnChartValueSelectedListener(this);
+
+        // The pieChart is built with animated from left to right
+        pieChart.animateXY(1400, 1400);
+        // white space between the slices
+        dataSet.setSliceSpace(2f);
+        // onClick the slices shift / enlarge
+        dataSet.setSelectionShift(5f);
+        // For lines before the values
+        dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        dataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+
+
     }
+
+    @Override
+    public void onValueSelected(Entry e,   Highlight h) {
+
+        if (e == null)
+            return;
+        Log.i("VAL SELECTED",
+                "Value: " + e.getVal() + ", xIndex: " + e.getXIndex()
+                        + ", DataSet index: " + dataSetIndex);
+    }
+
+
+    @Override
+    public void onNothingSelected() {
+        Log.i("PieChart", "nothing selected");
+    }
+
 }

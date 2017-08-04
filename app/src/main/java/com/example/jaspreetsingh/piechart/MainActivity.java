98eb8e7e285e287d22cc55854331cac54244e8d3
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2017-08-04 12:21:09 +0530

    Built an pieChart

--- app/src/main/java/com/example/jaspreetsingh/piechart/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/piechart/MainActivity.java
@@ -1,95 +1,41 @@
 package com.example.jaspreetsingh.piechart;
 
-import android.graphics.Color;
+;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements OnChartValueSelectedListener {
+public class MainActivity extends AppCompatActivity {
+
+    float sales[] = {30f, 20f, 40f, 6f, 4f};
+    String priorityLables[] = {"Hot+", "Hot", "Warm", "Cold", "Shortfall"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PieChart pieChart = (PieChart) findViewById(R.id.piechart);
-        pieChart.setUsePercentValues(true);
-
-        // IMPORTANT: In a PieChart, no values (Entry) should have the same
-        // xIndex (even if from different DataSets), since no values can be
-        // drawn above each other.
-        ArrayList<Entry> yvalues = new ArrayList<Entry>();
-        yvalues.add(new Entry(1f, 0));
-        yvalues.add(new Entry(1f, 1));
-        yvalues.add(new Entry(27f, 2));
-        yvalues.add(new Entry(38f, 3));
-        yvalues.add(new Entry(33f, 4));
-
-        PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
-
-        ArrayList<String> xVals = new ArrayList<String>();
-
-        xVals.add("January");
-        xVals.add("February");
-        xVals.add("March");
-        xVals.add("April");
-        xVals.add("May");
-
-        PieData data = new PieData(xVals, dataSet);
-        data.setValueFormatter(new PercentFormatter());
-        pieChart.setData(data);
-        pieChart.setDescription("This is Pie Chart");
-
-        // Disable the rotation of the pie chart
-        pieChart.setRotationEnabled(false);
-
-        // Disable the hole in the pieChart
-        pieChart.setDrawHoleEnabled(false);
-        pieChart.setTransparentCircleRadius(25f);
-        pieChart.setHoleRadius(25f);
-
-        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
-        data.setValueTextSize(13f);
-        data.setValueTextColor(Color.DKGRAY);
-        pieChart.setOnChartValueSelectedListener(this);
-
-        // The pieChart is built with animated from left to right
-        pieChart.animateXY(1400, 1400);
-        // white space between the slices
-        dataSet.setSliceSpace(2f);
-        // onClick the slices shift / enlarge
-        dataSet.setSelectionShift(5f);
-        // For lines before the values
-        dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-        dataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-
-
+        
+        setupPieChart();
     }
 
-    @Override
-    public void onValueSelected(Entry e,   Highlight h) {
+    private void setupPieChart() {
+        // List for the Pie chart PieEnteries
+        ArrayList<PieEntry> pieEnteries = new ArrayList<>();
+        for(int i = 0; i < sales.length; i++)   {
+            pieEnteries.add(new PieEntry(sales[i], priorityLables[i]));
+        }
+        PieDataSet dataSet = new PieDataSet(pieEnteries, "");
+        PieData data = new PieData(dataSet);
 
-        if (e == null)
-            return;
-        Log.i("VAL SELECTED",
-                "Value: " + e.getVal() + ", xIndex: " + e.getXIndex()
-                        + ", DataSet index: " + dataSetIndex);
+        // Get the chart
+        PieChart chart = (PieChart) findViewById(R.id.pie_chart);
+        chart.setData(data);
+        chart.invalidate(); // for redraw
     }
-
-
-    @Override
-    public void onNothingSelected() {
-        Log.i("PieChart", "nothing selected");
-    }
-
 }

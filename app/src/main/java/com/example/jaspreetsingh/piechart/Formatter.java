13cf9c7ee8299a98582bd9d9c3c2e120ef0d89f1
jaspreet.singh <cgc.jsbaidwan@gmail.com>
2017-08-07 11:01:23 +0530

    Custom Formatter for casting values to integer

--- app/src/main/java/com/example/jaspreetsingh/piechart/Formatter.java
+++ app/src/main/java/com/example/jaspreetsingh/piechart/Formatter.java
@@ -11,6 +11,7 @@ import com.github.mikephil.charting.utils.ViewPortHandler;
 public class Formatter implements IValueFormatter {
     @Override
     public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+        // Casting value into to integer
         return "" + ((int) value);
     }
 }
